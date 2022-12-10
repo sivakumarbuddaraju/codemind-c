12857b64e7a904cd02bc74b78b99091ed7c68ae9
sivakumarbuddaraju <119720292+sivakumarbuddaraju@users.noreply.github.com>
2022-12-10 11:40:51 +0530

    #include<stdio.h> int main() {     int n,sum=0;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2==1)         {             sum=sum+a[i];         }     }     printf("%d",sum); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==1)
+        {
+            sum=sum+a[i];
+        }
+    }
+    printf("%d",sum);
+}
