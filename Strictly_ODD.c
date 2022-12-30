2c3a525ce4a18833f1925e7a31326067249b08aa
sivakumarbuddaraju <119720292+sivakumarbuddaraju@users.noreply.github.com>
2022-12-30 10:25:07 +0530

    #include<stdio.h> int main() {     int n,i,c=0,s=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==1)         {             c++;         }         if(a[i]%2==1&& i%2==1)         {             s++;         }     }     if(c==s)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0,s=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==1)
+        {
+            c++;  
+        }
+        if(a[i]%2==1&& i%2==1)
+        {
+            s++;  
+        }
+    }
+    if(c==s)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
