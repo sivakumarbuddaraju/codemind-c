a36483faabfd3ca75bb451e6e6dbb46a449a47d8
sivakumarbuddaraju <119720292+sivakumarbuddaraju@users.noreply.github.com>
2023-01-06 09:50:49 +0530

    #include<stdio.h> int main() {     int n,total=0,i;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(n%i==0)         {             total=total+i;         }     }     printf("%d",total); }

---
+++ _sum_of_factors.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,total=0,i;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            total=total+i;
+        }
+    }
+    printf("%d",total);
+}
